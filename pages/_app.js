58f92ccd79e7a98fcbd76d6eaf3e357199b0adcd
nonlin-lin-chaos-order-etc-etal <19966907+nonlin-lin-chaos-order-etc-etal@users.noreply.github.com>
2021-10-30 22:51:51 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,7 @@
 import '../styles/globals.css'
-import global_styles from '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
 export default MyApp
-export default global_styles
