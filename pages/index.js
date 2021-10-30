271e5fb43af9cbcb3d5aa77bcdce1424d66d79e2
nonlin-lin-chaos-order-etc-etal <19966907+nonlin-lin-chaos-order-etc-etal@users.noreply.github.com>
2021-10-30 22:52:50 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -53,7 +53,7 @@ export default function Home() {
       </main>
 
       <footer className={styles.footer}>
-        <p className={global_styles.footerline}>
+        <p className={footerline}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
@@ -65,7 +65,7 @@ export default function Home() {
           </span>
         </a>
         </p>
-        <p className={global_styles.footerline}>
+        <p className={footerline}>
         <a
           href="https://github.com/nonlin-lin-chaos-order-etc-etal/websofmath_now_sh_nextjs"
           target="_blank"
